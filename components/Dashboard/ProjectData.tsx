13ae3588e0c2b9541e3536b8cfa2abe20af34cb8
Chirag Panjwani <chirag.panjwani@genesistechnologies.in>
2024-08-13 20:17:19 +0530

    designed the layout of dashboard

---
+++ components/Dashboard/ProjectData.tsx
@@ -0,0 +1,5 @@
+export default function ProjectData() {
+    return(
+        <h1>This is Project Data</h1>
+    )
+}
---
+++ components/Dashboard/Skills.tsx
@@ -0,0 +1,5 @@
+export default function Skills() {
+    return(
+        <h1>This is Skills</h1>
+    )
+}
