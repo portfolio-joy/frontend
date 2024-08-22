4bf4a1fa2a3a475957eca477ade1454bd6df00f2
Chirag Panjwani <chirag.panjwani@genesistechnologies.in>
2024-08-22 20:58:34 +0530

    Created the dummy list consisting of the user data entered

--- components/Dashboard/Skills.tsx
+++ components/Dashboard/Skills.tsx
@@ -1,5 +1,31 @@
+import styles from '@/styles/Dashboard.module.css'
+import { Chip, Divider, Radio, RadioGroup } from '@nextui-org/react'
+import { CrossIcon } from '../icons'
+import { useState } from 'react';
+
 export default function Skills() {
-    return(
-        <h1>This is Skills</h1>
+    const [skillType, setSkillType] = useState<string>("");
+    return (
+        <section className={styles['skills-section']}>
+            <div>
+                <Chip className={styles['skill-chip']}>
+                    <span>Skill Name</span>
+                    <button><CrossIcon /></button>
+                </Chip>
+            </div>
+            <Divider />
+            <form className={styles['dashboard-form']}>
+                <h2>Skills Form</h2>
+                <input className={styles['input-normal']} name='name' type='text' placeholder='Name'></input>
+                <RadioGroup orientation="horizontal" value={skillType} onValueChange={setSkillType}>
+                    <Radio value="Technical">Technical</Radio>
+                    <Radio value="Soft">Soft</Radio>
+                </RadioGroup>
+                {
+                    skillType==='Technical' && 
+                    <input className={styles['input-normal']} type='number' placeholder='Proficiency' max={100}></input>
+                }
+            </form>
+        </section>
     )
 }
