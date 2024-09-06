e129ff9287aff33389b29870b6c1d3f349232bd0
Chirag Panjwani <chirag.panjwani@genesistechnologies.in>
2024-09-06 20:23:29 +0530

    resolved the font issue in resume and updated the design of resume

--- components/Resume/ResumeDocument.tsx
+++ components/Resume/ResumeDocument.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
 import { UserResponseType } from '@/types/UserResponseType';
 
+Font.register({
+  family: 'Tinos',
+  src: 'http://fonts.gstatic.com/s/tinos/v9/EqpUbkVmutfwZ0PjpoGwCg.ttf'
+});
 
 const styles = StyleSheet.create({
   page: {
-    padding: 30,
+    fontFamily: 'Tinos'
   },
   header: {
+    padding:'0 30',
     fontSize: 20,
     borderBottom: '1px solid grey',
-    paddingBottom: 5,
     marginBottom: 10
   },
   section: {
@@ -19,33 +23,41 @@ const styles = StyleSheet.create({
   },
   username: {
     fontSize: 24,
-    marginBottom: 10,
+    padding: 10,
     fontWeight: 700,
     textAlign: 'center',
+    backgroundColor: '#345',
+    color: 'white'
   },
   contact: {
     flexDirection: 'row',
     borderBottom: '1px',
-    paddingBottom: 10,
+    padding: 10,
     justifyContent: 'center',
     flexWrap: 'wrap',
-    marginBottom: 20
+    marginBottom: 20,
+    backgroundColor: '#345',
+    color: 'white'
   },
   contactInfo: {
     fontSize: 12,
     paddingRight: 10,
   },
   subheader: {
+    padding: '0 30',
     fontSize: 16,
     marginBottom: 5,
   },
   text: {
+    padding: '0 30',
     fontSize: 12,
     marginBottom: 10,
   },
   listItem: {
+    display: 'flex',
+    padding: '10 20',
     fontSize: 12,
-    marginLeft: 10,
+    marginLeft: 5,
     marginBottom: 2,
   },
   contactDetail: {
@@ -100,10 +112,10 @@ export default function ResumeDocument(user: UserResponseType) {
         </View>
         <View style={styles.section}>
           <Text style={styles.header}>Skills</Text>
-          <View>
+          <View style={{display:'flex', flexDirection:'row', flexWrap: 'wrap'}}>
             {
               user?.skills?.map((skills, index) =>
-                <Text key={index} style={styles.listItem}>{skills.name}</Text>
+                <Text key={index} style={styles.listItem}>&#8224;  {skills.name}</Text>
               )
             }
           </View>
