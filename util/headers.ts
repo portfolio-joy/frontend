9daaa147d7f0aef9b37cd243dba0f39b5005df9b
Chirag Panjwani <panjwanichirag123@gmail.com>
2025-09-05 00:29:08 +0530

    Corrected header.ts

--- util/headers.ts
+++ util/headers.ts
@@ -1,5 +1,5 @@
 export function CommonHeaders() {
     return {
-        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_PORTFOLIO_URL
+        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_PORTFOLIO_URL ? process.env.NEXT_PUBLIC_PORTFOLIO_URL : 'http://localhost:3000'
     }
 }
